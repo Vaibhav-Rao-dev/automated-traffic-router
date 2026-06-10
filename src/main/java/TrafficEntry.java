06bb56326af786b18390af499b36b605ef92c91e
Vaibhav Rao <83571815+Vaibhav-Rao-dev@users.noreply.github.com>
2026-06-10 07:10:42 +0530

    Create TrafficEntry class with attributes and getters

---
+++ src/main/java/TrafficEntry.java
@@ -0,0 +1,12 @@
+public class TrafficEntry {
+    private String city;
+    private String service_type;
+    private String tool_name;
+    private String affiliate_link;
+
+    // Getters and Setters
+    public String getCity() { return city; }
+    public String getService_type() { return service_type; }
+    public String getTool_name() { return tool_name; }
+    public String getAffiliate_link() { return affiliate_link; }
+}
